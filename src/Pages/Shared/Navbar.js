3c2a8dfad2ff23d9a9d98e1cfb6c886e702cd9ae
nahid770 <nurnahid10@gmail.com>
2022-05-18 16:11:04 +0600

    navber added to the website

--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a></a></li>
+        <li><a>Home</a></li>
         <li tabindex="0">
           <a class="justify-between">
             Parent
